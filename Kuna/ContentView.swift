980235a931ea695581bc5331093561e4f26a63c4
Richard Annand <richard@ohheyrj.co.uk>
2025-08-15 14:22:30 +0100

    Initial Commit

---
+++ Kuna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kuna
+//
+//  Created by Richard Annand on 15/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kuna/KunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KunaApp.swift
+//  Kuna
+//
+//  Created by Richard Annand on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
