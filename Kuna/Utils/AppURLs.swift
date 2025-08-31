2b944b2f051da6f0011293e650427dfc33d2437d
Richard Annand <richard@ohheyrj.co.uk>
2025-08-31 17:32:44 +0100

    feat: add AppURLs for Kuna related and external URLs

---
+++ Kuna/Utils/AppURLs.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum AppURLs {
+    // Kuna related URLs
+    static let privacy = URL(string: "https://trykuna.app/privacy.html")! // swiftlint:disable:this force_unwrapping
+    static let license = URL(string: "https://trykuna.app/license.html")! // swiftlint:disable:this force_unwrapping
+
+    // External URLs
+    static let vikunja = URL(string: "https://vikunja.io")! // swiftlint:disable:this force_unwrapping
+    
+}
